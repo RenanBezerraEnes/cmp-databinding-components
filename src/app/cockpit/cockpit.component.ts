8170627b7d96f1f2d04858bc0549416b65c13668
RenanBezerraEnes <renanbezerraenes@icloud.com>
2023-09-28 14:46:30 +0200

    Here I am emmiting the events to the parent component.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,34 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css']
 })
-export class CockpitComponent implements OnInit {
+export class CockpitComponent {
   newServerName = '';
   newServerContent = '';
 
-  constructor() { }
+  
+  @Output() 
+  serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
-  ngOnInit() {
-  }
+
+  @Output() 
+  bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
   
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
   }
 
   onAddBlueprint() {
-  //   this.serverElements.push({
-  //     type: 'blueprint',
-  //     name: this.newServerName,
-  //     content: this.newServerContent
-  //   });
+    this.bluePrintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
   }
 
 }
